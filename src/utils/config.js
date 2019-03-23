cd85a6cb03e7eeb620476f40bddb2a864e2a45e4
ZhaiXiaoyuan <2549004118@qq.com>
2019-03-23 10:40:54 +0800

    20190323

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,10 +4,9 @@ export default {
     install: function (Vue, options) {
         let isDev=process.env.NODE_ENV=='development';
         Vue.appConfig={
-            domain:'http://api.linqi.tech',
-          /*  domain:isDev?'http://api.linqi.tech':'http://api.globalmazu.org:8801',*/
-            domain2:isDev?'http://api.linqi.tech:8600':'',
-            fileDomain:isDev?'http://api.linqi.tech':'',
+            domain:isDev?'http://api.linqi.tech':'http://api.linqi.tech',
+            domain2:isDev?'http://api.linqi.tech:8600':'http://api.linqi.tech:8600',
+            fileDomain:isDev?'http://api.linqi.tech':'http://api.linqi.tech',
         }
     },
 }
